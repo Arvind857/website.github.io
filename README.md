7881df8523ddc4a9107de1bddd6b0709c4fddfd6
Arvind kumar <76869940+Arvind857@users.noreply.github.com>
2021-01-22 16:28:01 +0530

    Initialize main

---
+++ README.md
@@ -0,0 +1,1 @@
+# arvindmoderen.github.io
---
+++ index.js
@@ -0,0 +1,60 @@
+var i = 0;
+var autoText = 'https : //dl.dropbox.com /s /bkj0q19h7jje6bn /        _20201223_173558%20%281%29.jpgdl=0'
+var speed = 100;
+
+function autoWriter() {
+
+  if (i < autoText.length) {
+    document.write(autoText.fontcolor("red"));
+    i++;
+    setTimeout(autoWriter,speed);
+  }
+//document.body.style.backgroundImage = "url('https://dl.dropbox.com/s/bkj0q19h7jje6bn/IMG_20201223_173558%20%281%29.jpgdl=0')"
+
+var toggle = document.body;
+  toggle.style.backgroundColor = toggle.style.backgroundColor == "white" ? "black" : "white";
+
+var audio = new Audio ("https://dl.dropbox.com/s/mcd0txzfdv81p8f/medium-pace-Typing-on-mechanical-keyboard-1-www.FesliyanStudios.com.mp3dl=0");
+
+audio.volume=0.5;
+
+
+
+audio.play();
+
+setTimeout(auto,8000);
+
+}
+
+function auto(){
+
+var text = 'You are hacked...👩‍💻\n\n Access Granted...\n\n Do not press back button or else I will format your device...\n\n Lets start the process...\n\n All paswords copied : ** ** * \n\n Pasword of credit & debit card : *,* \n\n Debit Card no : xxxx xxxx xxxx \n\n //Self Destruction 7 injected \n\n Device detected : Android \n\n //Installed Malware \n\n Location found.Tracking SIM card.. \n\n //Configuring the Update \n\n Ip Adress Found : 192.168.0.1 \n\n Amount credited : $0.014 \n\n Amount debited : $1362.78 \n\n Now You are ruined...$€£ \n\n https : //dl.dropbox.com /s /bkj0q19h7jje6bn /        _20201223_173558%20%281%29.jpgdl=0'
+
+var i = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, i);
+    i++;
+
+    if (i > text.length ) {
+        i = 0
+    }
+}
+setInterval(writeText, 80)
+
+
+ 
+ document.body.style.backgroundImage = "url('https://dl.dropbox.com/s/bkj0q19h7jje6bn/IMG_20201223_173558%20%281%29.jpgdl=0')"
+ 
+ document.body.style.color="orange"
+
+
+}
+
+function myFunction() {
+  setTimeout(function(){ alert("This is a prank... \n\n 😜 \n\n Don't get woried...\n\n #peace...✌\n\n Special Thanks to Aayush Hero ★★★  for the idea..."); }, 57000);
+}
+
+myFunction();
+alert('HELLO, I AM ARVIND KUMAR');
+alert('like share and subcribe youtube channel')
